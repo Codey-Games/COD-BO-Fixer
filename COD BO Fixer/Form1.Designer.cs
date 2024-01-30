11e7c333df7774b51ec1d6b6775f36cfbd90d056
cod.y <139372643+Codey-Games@users.noreply.github.com>
2024-01-31 03:54:25 +1300

    Add project files.

---
+++ COD BO Fixer/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace COD_BO_Fixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            label2 = new Label();
+            button3 = new Button();
+            label3 = new Label();
+            checkBox2 = new CheckBox();
+            button4 = new Button();
+            button5 = new Button();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 27);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Example: C:\\SteamLibrary\\Call of Duty Black Ops\\BlackOps.exe";
+            textBox1.Size = new Size(411, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(214, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Please enter the path to the executable:";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new Point(12, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(217, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Apply Patch";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new Point(235, 92);
+            button2.Name = "button2";
+            button2.Size = new Size(217, 23);
+            button2.TabIndex = 3;
+            button2.TabStop = false;
+            button2.Text = "Remove Patch";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Enabled = false;
+            checkBox1.Location = new Point(12, 56);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 4;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Correct File";
+            label2.TextChanged += label2_TextChanged;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(429, 27);
+            button3.Name = "button3";
+            button3.Size = new Size(23, 23);
+            button3.TabIndex = 6;
+            button3.Text = "+";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(354, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Patch Applied";
+            label3.TextAlign = ContentAlignment.TopRight;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Enabled = false;
+            checkBox2.Location = new Point(437, 55);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(15, 14);
+            checkBox2.TabIndex = 7;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Enabled = false;
+            button4.Location = new Point(12, 121);
+            button4.Name = "button4";
+            button4.Size = new Size(217, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Create Shortcuts";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Enabled = false;
+            button5.Location = new Point(235, 121);
+            button5.Name = "button5";
+            button5.Size = new Size(217, 23);
+            button5.TabIndex = 10;
+            button5.Text = "Remove Shortcuts";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.ControlDark;
+            label4.Location = new Point(367, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(85, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Made by cod.y";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(464, 158);
+            Controls.Add(label4);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(label3);
+            Controls.Add(checkBox2);
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
+        private Label label2;
+        private Button button3;
+        private Label label3;
+        private CheckBox checkBox2;
+        private Button button4;
+        private Button button5;
+        private Label label4;
+    }
+}
---
+++ COD BO Fixer/Form1.cs
@@ -0,0 +1,315 @@
+using System.Diagnostics.Eventing.Reader;
+using static System.Net.Mime.MediaTypeNames;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace COD_BO_Fixer
+{
+    public partial class Form1 : Form
+    {
+        bool correctFile;
+        bool patchedFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Call of Duty: Black Ops - Fixer";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            // check if its the correct file type (if its blackops.exe, if the path is over 4 characters long, idk, if the second character is :, if the third character is \)
+            if (text.EndsWith("BlackOps.exe") && text.Length > 4 && char.IsLetter(text[0]) && text[1] == ':' && text[2] == '\\')
+            {
+                correctFile = true;
+                checkBox1.Checked = true;
+            }
+            else
+            {
+                correctFile = false;
+                checkBox1.Checked = false;
+            }
+
+
+
+
+            // check if the file is patched
+            string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? ""; // if its not a null value ?? if it is a null value
+            if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+            {
+                string filePath = System.IO.Path.Combine(folderPath, "bgt5launcher.exe");
+                bool isFileExists = System.IO.File.Exists(filePath);
+
+                if (isFileExists && correctFile == true)
+                {
+                    patchedFile = true;
+                    checkBox2.Checked = true;
+                }
+                else
+                {
+                    patchedFile = false;
+                    checkBox2.Checked = false;
+                }
+            }
+            else
+            {
+                patchedFile = false;
+                checkBox2.Checked = false;
+            }
+
+
+
+
+
+            if (correctFile == true)
+            {
+                if (patchedFile == true)
+                {
+                    button5.Enabled = true;
+                    button4.Enabled = true;
+                    button2.Enabled = true;
+                    button1.Enabled = false;
+                }
+                else
+                {
+                    button5.Enabled = false;
+                    button4.Enabled = false;
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                }
+            }
+            else
+            {
+                button5.Enabled = false;
+                button4.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+
+
+        }
+
+        private void label2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Executable Files (*.exe)|*.exe|All Files (*.*)|*.*";
+            openFileDialog.Title = "Select an Executable File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Remove Patch
+
+            string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? ""; // if its not a null value ?? if it is a null value
+            if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+            {
+
+                string newFolderPath = System.IO.Path.Combine(folderPath, "OriginalFiles");
+
+                if (System.IO.Directory.Exists(newFolderPath)) // Check if the folder doesn't exist
+                {
+
+                    string filePath = System.IO.Path.Combine(newFolderPath, "steam_api.dll");
+                    bool isFileExists = System.IO.File.Exists(filePath);
+
+                    if (isFileExists)
+                    {
+                        System.IO.File.Delete(System.IO.Path.Combine(folderPath, "steam_api.dll"));
+
+                        // Move the file to the backup folder
+                        System.IO.File.Move(filePath, System.IO.Path.Combine(folderPath, "steam_api.dll"));
+
+                        // Delete the folder
+                        System.IO.Directory.Delete(newFolderPath);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Original Steam Integration cannot be found! Press OK to skip.");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Some original files cannot be found! Press OK to skip.");
+                }
+
+
+
+                string sourceFolderPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources");
+
+
+
+                string[] files = System.IO.Directory.GetFiles(sourceFolderPath);
+
+                // Copy each file to the destination folder
+                foreach (string patchFile in files)
+                {
+                    string destinationFilePath = System.IO.Path.Combine(folderPath, System.IO.Path.GetFileName(patchFile));
+
+                    // Delete the file
+                    if (System.IO.Path.GetFileName(patchFile) != "steam_api.dll")
+                    {
+                        System.IO.File.Delete(destinationFilePath);
+                    }
+                }
+
+                MessageBox.Show("Patch removed successfully!");
+
+                button2.Enabled = false;
+                button1.Enabled = true;
+                checkBox2.Checked = false;
+                button5.Enabled = false;
+                button4.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Apply Patch
+
+            string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? ""; // if its not a null value ?? if it is a null value
+            if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+            {
+
+                string newFolderPath = System.IO.Path.Combine(folderPath, "OriginalFiles");
+
+                if (!System.IO.Directory.Exists(newFolderPath)) // Check if the folder doesn't exist
+                {
+                    System.IO.Directory.CreateDirectory(newFolderPath); // Create the folder
+                }
+
+
+                string filePath = System.IO.Path.Combine(folderPath, "steam_api.dll");
+                bool isFileExists = System.IO.File.Exists(filePath);
+
+                if (isFileExists)
+                {
+                    System.IO.File.Move(filePath, System.IO.Path.Combine(newFolderPath, "steam_api.dll"));
+                }
+
+
+
+                string sourceFolderPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources");
+
+
+
+                string[] files = System.IO.Directory.GetFiles(sourceFolderPath);
+
+                // Copy each file to the destination folder
+                foreach (string patchFile in files)
+                {
+                    string fileName = System.IO.Path.GetFileName(patchFile);
+                    string destinationFilePath = System.IO.Path.Combine(folderPath, fileName);
+
+                    // Copy the file
+                    System.IO.File.Copy(patchFile, destinationFilePath);
+                }
+
+                MessageBox.Show("Patch applied successfully!");
+
+                button2.Enabled = true;
+                button1.Enabled = false;
+                checkBox2.Checked = true;
+                button5.Enabled = true;
+                button4.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // create shortcuts
+            string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? ""; // if its not a null value ?? if it is a null value
+            if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+            {
+
+                string filePath = System.IO.Path.Combine(folderPath, "bgt5launcher.exe");
+
+                // get start menu path
+                string startMenuProgramsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs");
+                string startMenuProgramsTargetPath = System.IO.Path.Combine(startMenuProgramsPath, "Start Server.lnk");
+
+                // get desktop path
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string desktopTargetPath = System.IO.Path.Combine(desktopPath, "Start Server.lnk");
+
+                //make desktop shortcut
+                WshShell shellDesktop = new WshShell();
+                IWshShortcut shortcutDesktop = (IWshShortcut)shellDesktop.CreateShortcut(desktopTargetPath);
+                shortcutDesktop.TargetPath = filePath;
+                shortcutDesktop.Save();
+
+                // make start menu shortcut
+                WshShell shellStartMenu = new WshShell();
+                IWshShortcut shortcutStartMenu = (IWshShortcut)shellStartMenu.CreateShortcut(startMenuProgramsTargetPath);
+                shortcutStartMenu.TargetPath = filePath;
+                shortcutStartMenu.Save();
+
+                MessageBox.Show("Shortcuts created successfully!");
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // remove shortcuts
+            string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? ""; // if its not a null value ?? if it is a null value
+            if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+            {
+
+                // get start menu path
+                string startMenuProgramsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs");
+                string startMenuProgramsTargetPath = System.IO.Path.Combine(startMenuProgramsPath, "Start Server.lnk");
+                if (System.IO.File.Exists(startMenuProgramsTargetPath))
+                {
+                    System.IO.File.Delete(startMenuProgramsTargetPath);
+                }
+                else
+                {
+                    MessageBox.Show("Start Menu Shortcut could not be found! Press OK to skip.");
+                }
+
+                // get desktop path
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string desktopTargetPath = System.IO.Path.Combine(desktopPath, "Start Server.lnk");
+                if (System.IO.File.Exists(desktopTargetPath))
+                {
+                    System.IO.File.Delete(desktopTargetPath);
+                }
+                else
+                {
+                    MessageBox.Show("Desktop Shortcut could not be found! Press OK to skip.");
+                }
+
+                MessageBox.Show("Shortcuts removed successfully!");
+
+            }
+        }
+    }
+}
