2fe54c48fd11567bb7452026027788e6fa246e53
cod.y <139372643+Codey-Games@users.noreply.github.com>
2024-02-01 17:17:23 +1300

    1.2

--- COD BO Fixer/Form1.Designer.cs
+++ COD BO Fixer/Form1.Designer.cs
@@ -41,21 +41,33 @@
             button4 = new Button();
             button5 = new Button();
             label4 = new Label();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            label5 = new Label();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            progress = new ProgressBar();
             SuspendLayout();
             // 
             // textBox1
             // 
+            textBox1.AllowDrop = true;
             textBox1.Location = new Point(12, 27);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "Example: C:\\SteamLibrary\\Call of Duty Black Ops\\BlackOps.exe";
-            textBox1.Size = new Size(411, 23);
+            textBox1.Size = new Size(382, 23);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlLightLight;
             label1.Font = new Font("Segoe UI", 9F);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
             label1.Location = new Point(12, 9);
             label1.Name = "label1";
             label1.Size = new Size(214, 15);
@@ -66,7 +78,7 @@
             // button1
             // 
             button1.Enabled = false;
-            button1.Location = new Point(12, 92);
+            button1.Location = new Point(12, 56);
             button1.Name = "button1";
             button1.Size = new Size(217, 23);
             button1.TabIndex = 2;
@@ -77,7 +89,7 @@
             // button2
             // 
             button2.Enabled = false;
-            button2.Location = new Point(235, 92);
+            button2.Location = new Point(235, 56);
             button2.Name = "button2";
             button2.Size = new Size(217, 23);
             button2.TabIndex = 3;
@@ -90,7 +102,7 @@
             // 
             checkBox1.AutoSize = true;
             checkBox1.Enabled = false;
-            checkBox1.Location = new Point(12, 56);
+            checkBox1.Location = new Point(371, 10);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(15, 14);
             checkBox1.TabIndex = 4;
@@ -99,7 +111,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(26, 55);
+            label2.Location = new Point(385, 9);
             label2.Name = "label2";
             label2.Size = new Size(67, 15);
             label2.TabIndex = 5;
@@ -108,7 +120,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(429, 27);
+            button3.Location = new Point(400, 27);
             button3.Name = "button3";
             button3.Size = new Size(23, 23);
             button3.TabIndex = 6;
@@ -119,7 +131,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(354, 55);
+            label3.Location = new Point(15, 271);
             label3.Name = "label3";
             label3.Size = new Size(81, 15);
             label3.TabIndex = 8;
@@ -130,7 +142,7 @@
             // 
             checkBox2.AutoSize = true;
             checkBox2.Enabled = false;
-            checkBox2.Location = new Point(437, 55);
+            checkBox2.Location = new Point(98, 271);
             checkBox2.Name = "checkBox2";
             checkBox2.Size = new Size(15, 14);
             checkBox2.TabIndex = 7;
@@ -139,7 +151,7 @@
             // button4
             // 
             button4.Enabled = false;
-            button4.Location = new Point(12, 121);
+            button4.Location = new Point(9, 116);
             button4.Name = "button4";
             button4.Size = new Size(217, 23);
             button4.TabIndex = 9;
@@ -150,7 +162,7 @@
             // button5
             // 
             button5.Enabled = false;
-            button5.Location = new Point(235, 121);
+            button5.Location = new Point(232, 116);
             button5.Name = "button5";
             button5.Size = new Size(217, 23);
             button5.TabIndex = 10;
@@ -162,17 +174,119 @@
             // 
             label4.AutoSize = true;
             label4.ForeColor = SystemColors.ControlDark;
-            label4.Location = new Point(367, 9);
+            label4.Location = new Point(235, 9);
             label4.Name = "label4";
             label4.Size = new Size(85, 15);
             label4.TabIndex = 11;
             label4.Text = "Made by cod.y";
             // 
+            // button6
+            // 
+            button6.Enabled = false;
+            button6.Image = Properties.Resources.folder1;
+            button6.Location = new Point(429, 27);
+            button6.Name = "button6";
+            button6.Size = new Size(23, 23);
+            button6.TabIndex = 12;
+            button6.TextAlign = ContentAlignment.MiddleRight;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Enabled = false;
+            button7.Location = new Point(308, 267);
+            button7.Name = "button7";
+            button7.Size = new Size(141, 23);
+            button7.TabIndex = 13;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Enabled = false;
+            button8.Location = new Point(9, 145);
+            button8.Name = "button8";
+            button8.Size = new Size(440, 23);
+            button8.TabIndex = 14;
+            button8.Text = "Open Server Starter";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ControlLightLight;
+            label5.Font = new Font("Segoe UI", 9F);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.Location = new Point(12, 98);
+            label5.Name = "label5";
+            label5.Size = new Size(82, 15);
+            label5.TabIndex = 15;
+            label5.Text = "Other Settings";
+            // 
+            // button9
+            // 
+            button9.Enabled = false;
+            button9.Location = new Point(12, 295);
+            button9.Name = "button9";
+            button9.Size = new Size(217, 23);
+            button9.TabIndex = 17;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Enabled = false;
+            button10.Location = new Point(235, 295);
+            button10.Name = "button10";
+            button10.Size = new Size(217, 23);
+            button10.TabIndex = 16;
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Enabled = false;
+            button11.Location = new Point(12, 324);
+            button11.Name = "button11";
+            button11.Size = new Size(217, 23);
+            button11.TabIndex = 18;
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Enabled = false;
+            button12.Location = new Point(235, 324);
+            button12.Name = "button12";
+            button12.Size = new Size(217, 23);
+            button12.TabIndex = 19;
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // progress
+            // 
+            progress.Location = new Point(9, 174);
+            progress.Name = "progress";
+            progress.Size = new Size(440, 13);
+            progress.TabIndex = 20;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(464, 158);
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(464, 197);
+            Controls.Add(progress);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(label5);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
             Controls.Add(label4);
             Controls.Add(button5);
             Controls.Add(button4);
@@ -185,10 +299,11 @@
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Call of Duty: Black Ops - Fixer";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -208,5 +323,14 @@
         private Button button4;
         private Button button5;
         private Label label4;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Label label5;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private ProgressBar progress;
     }
 }
--- COD BO Fixer/Form1.cs
+++ COD BO Fixer/Form1.cs
@@ -2,6 +2,7 @@ using System.Diagnostics.Eventing.Reader;
 using static System.Net.Mime.MediaTypeNames;
 using IWshRuntimeLibrary;
 using System.IO;
+using System.Diagnostics;
 
 namespace COD_BO_Fixer
 {
@@ -22,7 +23,7 @@ namespace COD_BO_Fixer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Text = "Call of Duty: Black Ops - Fixer";
+            progress.Visible = false;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -81,13 +82,21 @@ namespace COD_BO_Fixer
             {
                 if (patchedFile == true)
                 {
-                    button5.Enabled = true;
-                    button4.Enabled = true;
-                    button2.Enabled = true;
-                    button1.Enabled = false;
+                    progress.Visible = true;
+                    progress.Value = 100; // progress bar
+                    button8.Enabled = true; // open server starter
+                    button6.Enabled = true; // open game folder
+                    button5.Enabled = true; // create/remove shortcut
+                    button4.Enabled = true; // create/remove shortcut
+                    button2.Enabled = true; // remove patch
+                    button1.Enabled = false; // apply patch
                 }
                 else
                 {
+                    progress.Value = 0; // progress bar
+                    progress.Visible = false;
+                    button8.Enabled = false;
+                    button6.Enabled = true;
                     button5.Enabled = false;
                     button4.Enabled = false;
                     button1.Enabled = true;
@@ -96,6 +105,10 @@ namespace COD_BO_Fixer
             }
             else
             {
+                progress.Value = 0; // progress bar
+                progress.Visible = false;
+                button8.Enabled = false;
+                button6.Enabled = false;
                 button5.Enabled = false;
                 button4.Enabled = false;
                 button1.Enabled = false;
@@ -151,13 +164,13 @@ namespace COD_BO_Fixer
                     }
                     else
                     {
-                        MessageBox.Show("Original Steam Integration cannot be found! Press OK to skip.");
+                        MessageBox.Show("Original Steam Integration cannot be found! Press OK to skip.", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
 
                 }
                 else
                 {
-                    MessageBox.Show("Some original files cannot be found! Press OK to skip.");
+                    MessageBox.Show("Some original files cannot be found! Press OK to skip.", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
 
@@ -180,13 +193,72 @@ namespace COD_BO_Fixer
                     }
                 }
 
-                MessageBox.Show("Patch removed successfully!");
 
+                progress.Visible = true;
+                progress.Value = 50; // progress bar
                 button2.Enabled = false;
                 button1.Enabled = true;
                 checkBox2.Checked = false;
                 button5.Enabled = false;
                 button4.Enabled = false;
+                button8.Enabled = false;
+                button6.Enabled = true;
+
+                MessageBox.Show("Patch removed successfully!");
+            }
+
+            
+            // remove shortcuts
+            if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+            {
+
+                // get start menu path
+                string startMenuProgramsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs");
+                string startMenuProgramsTargetPath = System.IO.Path.Combine(startMenuProgramsPath, "Start Server.lnk");
+
+                // get desktop path
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string desktopTargetPath = System.IO.Path.Combine(desktopPath, "Start Server.lnk");
+
+                if (System.IO.File.Exists(desktopTargetPath) || System.IO.File.Exists(startMenuProgramsTargetPath))
+                {
+                    DialogResult result = MessageBox.Show("You still have shortcuts created! Would you like to remove them?", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        if (System.IO.File.Exists(desktopTargetPath))
+                        {
+                            System.IO.File.Delete(desktopTargetPath);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Desktop Shortcut could not be found! Press OK to skip.", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        if (System.IO.File.Exists(startMenuProgramsTargetPath))
+                        {
+                            System.IO.File.Delete(startMenuProgramsTargetPath);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Start Menu Shortcut could not be found! Press OK to skip.", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                        progress.Value = 0; // progress bar
+                        progress.Visible = false;
+                        MessageBox.Show("Shortcuts removed successfully!");
+
+                    }
+                    else
+                    {
+                        progress.Value = 0; // progress bar
+                        progress.Visible = false;
+                    }
+                }
+                else
+                {
+                    progress.Value = 0; // progress bar
+                    progress.Visible = false;
+                }
             }
         }
 
@@ -213,6 +285,10 @@ namespace COD_BO_Fixer
                 {
                     System.IO.File.Move(filePath, System.IO.Path.Combine(newFolderPath, "steam_api.dll"));
                 }
+                else
+                {
+                    MessageBox.Show("Steam API not found! Press OK to skip.", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
 
 
 
@@ -232,13 +308,17 @@ namespace COD_BO_Fixer
                     System.IO.File.Copy(patchFile, destinationFilePath);
                 }
 
-                MessageBox.Show("Patch applied successfully!");
-
+                progress.Visible = true;
+                progress.Value = 100; // progress bar
                 button2.Enabled = true;
                 button1.Enabled = false;
                 checkBox2.Checked = true;
                 button5.Enabled = true;
                 button4.Enabled = true;
+                button8.Enabled = true;
+                button6.Enabled = true;
+
+                MessageBox.Show("Patch applied successfully!");
             }
         }
 
@@ -294,7 +374,7 @@ namespace COD_BO_Fixer
                 }
                 else
                 {
-                    MessageBox.Show("Start Menu Shortcut could not be found! Press OK to skip.");
+                    MessageBox.Show("Start Menu Shortcut could not be found! Press OK to skip.", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
                 // get desktop path
@@ -306,11 +386,136 @@ namespace COD_BO_Fixer
                 }
                 else
                 {
-                    MessageBox.Show("Desktop Shortcut could not be found! Press OK to skip.");
+                    MessageBox.Show("Desktop Shortcut could not be found! Press OK to skip.", "Call of Duty: Black Ops - Fixer", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
                 MessageBox.Show("Shortcuts removed successfully!");
 
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // open game folder
+            string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? "";
+
+            if (correctFile) // check if the path is not null or empty
+            {
+                System.Diagnostics.Process.Start("explorer.exe", folderPath);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // start game
+
+            if (correctFile)
+            {
+                if (correctFile && patchedFile)
+                {
+                    string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? ""; // if its not a null value ?? if it is a null value
+                    if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+                    {
+                        Process.Start(System.IO.Path.Combine(folderPath, "BlackOps.exe"));
+                    }
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // start server starter
+
+            if (correctFile && patchedFile)
+            {
+                string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? ""; // if its not a null value ?? if it is a null value
+                if (!string.IsNullOrEmpty(folderPath)) // check if the path is not null or empty
+                {
+                    Process.Start(System.IO.Path.Combine(folderPath, "bgt5launcher.exe"));
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // force close game
+
+            foreach (var process in Process.GetProcessesByName("BlackOps.exe"))
+            {
+                process.Kill();
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            // force close server starter
+
+            foreach (var process in Process.GetProcessesByName("BGamer: Black Ops"))
+            {
+                MessageBox.Show(process.ProcessName);
+                process.Kill();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // open server starter as admin
+
+            if (correctFile)
+            {
+                if (correctFile && patchedFile)
+                {
+                    string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? "";
+
+                    if (!string.IsNullOrEmpty(folderPath))
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            FileName = System.IO.Path.Combine(folderPath, "bgt5launcher.exe"),
+                            Verb = "runas" // Run as administrator
+                        };
+
+                        try
+                        {
+                            Process.Start(startInfo);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error starting the application: " + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // open game as admin
+
+            if (correctFile)
+            {
+                if (correctFile && patchedFile)
+                {
+                    string folderPath = System.IO.Path.GetDirectoryName(textBox1.Text) ?? "";
+
+                    if (!string.IsNullOrEmpty(folderPath))
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            FileName = System.IO.Path.Combine(folderPath, "BlackOps.exe"),
+                            Verb = "runas" // Run as administrator
+                        };
+
+                        try
+                        {
+                            Process.Start(startInfo);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error starting the application: " + ex.Message);
+                        }
+                    }
+                }
             }
         }
     }
